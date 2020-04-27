0fc1ddf61c393403551087ea0dccb29889a341e5
7usharnp <51354942+7usharnp@users.noreply.github.com>
2020-04-27 09:10:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+const express = require("express");
+const bodyParser= require("body-parser");
+let items = ["Do yoga.","watch ipl match"];
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+
+let today  = new Date();
+let options = { weekday: 'long',
+                month: 'long',
+                day: 'numeric' };
+
+
+
+let day = today.toLocaleDateString("en-US", options);
+
+
+
+res.render("list",{
+  kindOfDay: day, newListItems: items});
+
+});
+app.post("/" ,function(req,res){
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
+
+app.listen(3000, function(){
+  console.log("server started on port 3000.");
+})
